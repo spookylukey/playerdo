769e5df717e45752d54e571f9f3a1fad9aa1018e
Luke Plant <L.Plant.98@cantab.net>
2010-07-15 02:01:19 +0100

    Added setup and README etc.

---
+++ README.rst
@@ -0,0 +1,15 @@
+player_do
+=========
+
+I use several media players, sometimes at the same time (like 'shell-fm' running
+in a terminal, and something else with a GUI), and sometimes I try out new media
+players for a while. I want the the media buttons on my keyboard to Do The Right
+Thing without having to reprogram keyboard shortcuts.  This package is my
+solution to that problem.
+
+I set up keyboard shortcuts to map the play/pause button to 'player_do
+playpause' etc, and player_do generally does the Right Thing.
+
+The currently supported programs can be seen by running 'player_do help'.  To
+add more supported programs, see the existing code in the 'backends' directory.
+Patches gratefully received!
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+setup(
+    name = "player_do",
+    version = "0.1",
+    packages = find_packages(),
+    scripts = ['player_do'],
+
+    # metadata for upload to PyPI
+    author = "Luke Plant",
+    author_email = "L.Plant.98@cantab.net",
+    description = "Control various media players from a single command line interface.",
+    license = "BSD",
+    keywords = "music player command interface wrapper",
+    url = "http://bitbucket.org/spookylukey/playerdo",
+    classifiers = [
+        "Topic :: Multimedia :: Sound/Audio :: Players",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta"
+        ]
+)
