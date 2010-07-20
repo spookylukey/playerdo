0450bb623eee06ce30792852ff8e5396ae1d2bf8
Luke Plant <L.Plant.98@cantab.net>
2010-07-20 23:08:38 +0100

    Misc updates to setup.py, including a long description.

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
 setup(
     name = "playerdo",
-    version = "0.1",
+    version = "0.2",
     packages = find_packages(),
     scripts = ['player_do'],
 
@@ -10,12 +17,19 @@ setup(
     author = "Luke Plant",
     author_email = "L.Plant.98@cantab.net",
     description = "Control various media players from a single command line interface.",
+    long_description = (
+                        read('README.txt')
+                        + "\n\n" +
+                        read('CHANGES.txt')
+    ),
     license = "BSD",
     keywords = "music media MPRIS player command interface wrapper",
     url = "http://bitbucket.org/spookylukey/playerdo",
     classifiers = [
         "Topic :: Multimedia :: Sound/Audio :: Players",
         "License :: OSI Approved :: BSD License",
-        "Development Status :: 4 - Beta"
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: End Users/Desktop",
+        "Operating System :: POSIX :: Linux",
         ]
 )
