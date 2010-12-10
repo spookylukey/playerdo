6a4d30e65b225536f8c51247bf831a50ce4cc0b9
Luke Plant <L.Plant.98@cantab.net>
2010-12-10 12:37:33 +0000

    Fairly hacky but functional Python3 support via 2to3

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 import os
-
+import sys
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+    import distutils.util
+    import setuptools
+    from lib2to3.refactor import get_fixers_from_package
+    fixer_names = []
+    for p in setuptools.lib2to3_fixer_packages:
+        fixer_names.extend(get_fixers_from_package(p))
+
+    # Modify
+    fixer_names.remove('lib2to3.fixes.fix_next')
+    # Monkey patch:
+    distutils.util.Mixin2to3.fixer_names = fixer_names
 
 setup(
     name = "playerdo",
@@ -31,5 +46,15 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: End Users/Desktop",
         "Operating System :: POSIX :: Linux",
-        ]
+        ],
+    **extra
 )
+
+
+# dbus isn't on PyPI, can't just put it in requirements
+try:
+    import dbus
+except ImportError:
+    sys.stderr.write("WARNING: Python 'dbus' library is not installed. Some backends will not work.\n")
+    if sys.version_info >= (3,):
+        sys.stderr.write("At the time of writing, a Python 3 dbus library is only available if you build the py3k branch available at http://cgit.freedesktop.org/dbus/dbus-python/ \n")
