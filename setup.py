ca62a0545e1479c488c57b65d1debe934b327dd3
Luke Plant <L.Plant.98@cantab.net>
2012-10-11 17:29:50 +0100

    Added support for Python 3.2/3.3 in setup.py

--- setup.py
+++ setup.py
@@ -53,6 +53,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.0",
         "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
         ],
     **extra
 )
