53ee77ea3677fe7079a8ae9e853a31612dd61c2c
Luke Plant <L.Plant.98@cantab.net>
2014-03-08 22:01:06 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if sys.version_info >= (3,):
 
 setup(
     name = "playerdo",
-    version = "0.8",
+    version = "0.9",
     packages = find_packages(),
     include_package_data = True,
     scripts = ['player_do'],
