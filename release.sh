ac39b2e8e39a6f3088b783890818dfc8cd06b8b0
Luke Plant <luke@lukeplant.me.uk>
2024-10-19 11:10:59 +0100

    Fixed release script

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ git ls-tree --full-tree --name-only -r HEAD | xargs chmod ugo+r
 find src -type d | xargs chmod ugo+rx
 
 uv build --sdist --wheel || exit 1
-uv publish --sdist --wheel  || exit 1
+uv publish  || exit 1
 
 VERSION=$(uv pip show playerdo | grep 'Version: ' | cut -f 2 -d ' ' | tr -d '\n') || exit 1
 
